12a164e58dd8b8b0386a226012cb3657ed253e78
astax <augusttaku824@gmail.com>
2024-12-03 01:17:35 +0900

    マイグレーションファイルと乖離があったため、マイグレーションを再実行した。

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_23_073025) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_user_accounts_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_user_accounts_on_reset_password_token", unique: true
   end
 
   add_foreign_key "posts", "user_accounts"
